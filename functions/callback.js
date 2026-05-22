14b8c0deaad6916df6485efef939341019036a64
Admin-XQY <3131940292@qq.com>
2026-05-22 20:35:38 +0800

    用 Cloudflare Pages Functions 替代独立 Worker 处理 OAuth

---
+++ functions/callback.js
@@ -0,0 +1,42 @@
+export async function onRequest({ env, request }) {
+  const url = new URL(request.url);
+  const code = url.searchParams.get("code");
+  if (!code) {
+    return new Response("Missing code", { status: 400 });
+  }
+
+  const tokenRes = await fetch(
+    "https://github.com/login/oauth/access_token",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        client_id: env.OAUTH_CLIENT_ID,
+        client_secret: env.OAUTH_CLIENT_SECRET,
+        code,
+      }),
+    },
+  );
+  const data = await tokenRes.json();
+
+  const html = `<!doctype html><html><body><script>
+    (function() {
+      function recieveMessage(e) {
+        window.opener.postMessage(
+          'authorization:github:success:${JSON.stringify(data)}',
+          e.origin
+        );
+        window.removeEventListener("message", recieveMessage, false);
+      }
+      window.addEventListener("message", recieveMessage, false);
+      window.opener.postMessage("authorizing:github", "*");
+    })();
+  </script></body></html>`;
+
+  return new Response(html, {
+    headers: { "Content-Type": "text/html" },
+  });
+}
